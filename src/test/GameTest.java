b1e490a4342b9b1979c863b3b285c19382610a47
Daniel Ma <dma18@berkeley.edu>
2016-12-24 17:00:46 -0800

    Rearranging files

---
+++ src/test/GameTest.java
@@ -0,0 +1,31 @@
+import org.junit.*;
+/**
+ * Created by danie on 12/24/2016.
+ */
+class GameTest {
+    @Test
+    void generateMines() {
+
+    }
+
+    @Test
+    void revealSquare() {
+
+    }
+
+    @Test
+    void checkForMines() {
+
+    }
+
+    @Test
+    void flagSquare() {
+
+    }
+
+    @Test
+    void unflagSquare() {
+
+    }
+
+}
