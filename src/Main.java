2c191bd80c18889b986260a633c5c58a76471657
Daniel Ma <dma18@berkeley.edu>
2016-12-25 17:25:23 -0800

    Constructors for GUI

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
 /**
  * Created by danie on 12/23/2016.
  */
+import javax.swing.*;
+import java.awt.GridLayout;
 
 public class Main {
+
+    JFrame frame;
+
     public static void main(String[] args) {
 
+    }
+
+    void startGame(int width, int height, int numMines) {
+
+    }
+
+    void gameEasy() {
+        startGame(9, 9, 10);
+    }
+
+    void gameMedium() {
+        startGame(16, 16, 16);
+    }
+
+    void gameDifficult() {
+        startGame(30, 30, 30);
     }
 }
